3d1711a394108b32d06f9d294d21e490733be19e
tanmay-hire-tcgls <tanmay.hire@tcgre.com>
2024-03-18 01:18:35 +0530

    fetch call in backend to get categories and render-tested

--- src/app/configuration/page.tsx
+++ src/app/configuration/page.tsx
@@ -1,7 +1,29 @@
-'use client'
+/* eslint-disable @next/next/no-async-client-component */
+"use client";
 
-export default function Configuration () {
-    return (<div>
-        Configuration Page
-    </div>)
+export async function getCategoriesData() {
+  const response = await fetch("http://localhost:7007/categories");
+  return response.json();
+}
+
+export default async function Configuration() {
+  const categories = await getCategoriesData();
+
+  return (
+    <div>
+      <div>Configuration Page</div>
+      <div className="mt-5">
+        <div>
+          <div className="ml-2"> Categories </div>
+          <div>
+            {categories.map((cat: any) => (
+              <div className="ml-5" key={cat._id}>
+                {cat?.title}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
